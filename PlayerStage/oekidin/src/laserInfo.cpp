c1a664b858309e8e369830b42f4a9545fc9a2a51
Xabier Lahuerta Vázquez <xabilahu99@hotmail.com>
2020-10-27 12:16:25 +0100

    Obstacle Evading code, first approach

--- PlayerStage/oekidin/src/laserInfo.cpp
+++ PlayerStage/oekidin/src/laserInfo.cpp
@@ -4,10 +4,9 @@
 #include <libplayerc++/playerc++.h>
 #include <iostream>
 
-int
-main(int argc, const char **argv)
+int main(int argc, const char **argv)
 {
-  int i;  
+  int i;
 
   try
   {
@@ -19,68 +18,61 @@ main(int argc, const char **argv)
     LaserProxy laserra(&bezeroa, 0);
     // Sortu position2d interfazea eta harpidetu bezeroarekin
     Position2dProxy robota(&bezeroa, 0);
-    
+
     std::cout << bezeroa << std::endl;
-    
-    robota.SetMotorEnable (true);
+
+    robota.SetMotorEnable(true);
 
     for (i = 0; i < 10; i++)
       bezeroa.Read();
 
-    
     int count = laserra.GetCount();
-    
-    std::cout << "Scan count: "  <<  count << std::endl;    
-    std::cout << "Scan resolution: "  <<  laserra.GetScanRes() << std::endl;
-    std::cout << "Range resolution: " <<  laserra.GetRangeRes() << std::endl;
-    std::cout << "Max Range: " <<  laserra.GetMaxRange() << std::endl;
-    std::cout << "Max angle: " <<  laserra.GetMaxAngle() << std::endl;
-    std::cout << "Min angle: " <<  laserra.GetMinAngle() << std::endl;
+
+    std::cout << "Scan count: " << count << std::endl;
+    std::cout << "Scan resolution: " << laserra.GetScanRes() << std::endl;
+    std::cout << "Range resolution: " << laserra.GetRangeRes() << std::endl;
+    std::cout << "Max Range: " << laserra.GetMaxRange() << std::endl;
+    std::cout << "Max angle: " << laserra.GetMaxAngle() << std::endl;
+    std::cout << "Min angle: " << laserra.GetMinAngle() << std::endl;
 
     std::cout << "Bearings: " << std::endl;
     for (i = 0; i < count; i++)
-      {
-	std::cout << laserra.GetBearing(i) << ", ";
-      }
-    std::cout << "-----------------------------------" << std::endl;
-
-
-    while (1)
-      { 
-	float max = -1.0;
-	float min = laserra.GetMaxRange();
-	int maxind = -1;
-	int minind = -1;
-	bezeroa.Read();
-	for (i = 0; i < count; i++)
-	  {
-	    if (laserra.GetRange(i) > max && laserra.GetRange(i) != laserra.GetMaxRange())
-	      {
-		max = laserra.GetRange(i);
-		maxind = i;
-	      }
-	    if (laserra.GetRange(i) < min)
-	      {
-		min = laserra.GetRange(i);
-		minind = i;
-	      }
-	  }
-	// Print info
-	
-	std::cout << "Max Reading: " <<  max << " Index:" << maxind << std::endl;
-	std::cout << "Min Reading: " <<  min << " Index:" << minind << std::endl;
-	std::cout << "-----------------------------------" << std::endl;
-	// robota.SetSpeed(0, 0.2);
-      }
-  }
-  catch (PlayerCc::PlayerError & e)
     {
-      std::cerr << e << std::endl;
-      return -1;
+      std::cout << laserra.GetBearing(i) << ", ";
     }
+    std::cout << "-----------------------------------" << std::endl;
     
+    while (1)
+    {
+      float max = -1.0;
+      float min = laserra.GetMaxRange();
+      int maxind = -1;
+      int minind = -1;
+      bezeroa.Read();
+      for (i = 0; i < count; i++)
+      {
+        if (laserra.GetRange(i) > max && laserra.GetRange(i) != laserra.GetMaxRange())
+        {
+          max = laserra.GetRange(i);
+          maxind = i;
+        }
+        if (laserra.GetRange(i) < min)
+        {
+          min = laserra.GetRange(i);
+          minind = i;
+        }
+      }
+      // Print info
+
+      std::cout << "Max Reading: " << max << " Index:" << maxind << std::endl;
+      std::cout << "Min Reading: " << min << " Index:" << minind << std::endl;
+      std::cout << "-----------------------------------" << std::endl;
+      // robota.SetSpeed(0, 0.2);
+    }
+  }
+  catch (PlayerCc::PlayerError &e)
+  {
+    std::cerr << e << std::endl;
+    return -1;
+  }
 }
-
-
-
-
--- PlayerStage/oekidin/src/oztopoakEkiditen-TODO.cpp
+++ PlayerStage/oekidin/src/oztopoakEkiditen-TODO.cpp
@@ -5,22 +5,21 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib>
 
 std::ofstream datuFitx;
 std::string fileName = "outData.txt";
 
-void
-closeAll(int signum)
+void closeAll(int signum)
 {
   std::cout << "Interrupt signal (" << signum << ") received\n";
   //datuFitx.close();
   exit(signum);
 }
 
-int
-main(int argc, const char **argv)
+int main(int argc, const char **argv)
 {
-  int i;  
+  int i;
   signal(SIGINT, closeAll);
 
   try
@@ -33,48 +32,79 @@ main(int argc, const char **argv)
     LaserProxy laserra(&bezeroa, 0);
     // Sortu position2d interfazea eta harpidetu bezeroarekin
     Position2dProxy robota(&bezeroa, 0);
-    
+
     std::cout << bezeroa << std::endl;
-    
-    robota.SetMotorEnable (true);
+
+    robota.SetMotorEnable(true);
 
     for (i = 0; i < 10; i++)
       bezeroa.Read();
 
-    // Fitxategia prestatu 
+    // Fitxategia prestatu
     // datuFitx.open(fileName, ios::out);
     // datuFitx  << "# Robotaren (rx ry) koordenatuak \n";
 
     int count = laserra.GetCount();
-    
-    std::cout << "Scan count: "  <<  count << std::endl;    
-    std::cout << "Scan resolution: "  <<  laserra.GetScanRes() << std::endl;
-    std::cout << "Range resolution: " <<  laserra.GetRangeRes() << std::endl;
-    std::cout << "Max Range: " <<  laserra.GetMaxRange() << std::endl;
-    std::cout << "Max angle: " <<  laserra.GetMaxAngle() << std::endl;
-    std::cout << "Min angle: " <<  laserra.GetMinAngle() << std::endl;
+
+    std::cout << "Scan count: " << count << std::endl;
+    std::cout << "Scan resolution: " << laserra.GetScanRes() << std::endl;
+    std::cout << "Range resolution: " << laserra.GetRangeRes() << std::endl;
+    std::cout << "Max Range: " << laserra.GetMaxRange() << std::endl;
+    std::cout << "Max angle: " << laserra.GetMaxAngle() << std::endl;
+    std::cout << "Min angle: " << laserra.GetMinAngle() << std::endl;
+
+    int readingsPerSection = int(count / 3);
 
     while (1)
-      { 
-	bezeroa.Read();
-	
-	/* Laserraren irakurketak kudeatu */
-	/* Robotaren abiadurak finkatu */
-	float x = robota.GetXPos();
-	float y = robota.GetYPos();
-	//datuFitx  << x << " " << y  << "\n";
-
-	robota.SetSpeed(0, 0.2);
-      }
-  }
-  catch (PlayerCc::PlayerError & e)
     {
-      std::cerr << e << std::endl;
-      return -1;
+      double meanLeft = 0.0, meanForward = 0.0, meanRight = 0.0;
+      bezeroa.Read();
+
+      /* Laserraren irakurketak kudeatu */
+
+      for (int i = 0; i < count; i++)
+      {
+        int currentSection = int(i / readingsPerSection);
+        double reading = laserra.GetRange(i);
+        if (currentSection == 0)
+        {
+          meanRight += reading;
+        }
+        else if (currentSection == 1)
+        {
+          meanForward += reading;
+        }
+        else
+        {
+          meanLeft += reading;
+        }
+      }
+
+      meanLeft /= readingsPerSection;
+      meanForward /= readingsPerSection;
+      meanRight /= readingsPerSection;
+
+      // This piece of code aims to add noise into robot's decisions.
+      // if (fabs(meanLeft - meanRight) < 1.0) {
+      //   int multiplier = rand() % 2 - 1;
+      //   double shift = rand() % 1000 / 1000.0;
+      //   meanLeft += shift * multiplier;
+      //   meanRight += shift * (1 - multiplier);
+      // }
+
+      /* Robotaren abiadurak finkatu */
+      float x = robota.GetXPos();
+      float y = robota.GetYPos();
+      //datuFitx  << x << " " << y  << "\n";
+
+      printf("Forward: %.2f\tLeft: %.2f\tRight: %.2f\n", meanForward, meanLeft, meanRight);
+
+      robota.SetSpeed(meanForward / 100, (meanLeft - meanRight) / 20);
     }
-    
+  }
+  catch (PlayerCc::PlayerError &e)
+  {
+    std::cerr << e << std::endl;
+    return -1;
+  }
 }
-
-
-
-
