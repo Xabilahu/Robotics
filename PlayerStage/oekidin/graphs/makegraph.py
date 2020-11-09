eb128c4e60fa7f00c4f3e9d894a26243e513b019
Xabier Lahuerta Vázquez <xabilahu99@hotmail.com>
2020-11-09 11:06:47 +0100

    Graph rendering fixed

--- PlayerStage/oekidin/graphs/makegraph.py
+++ PlayerStage/oekidin/graphs/makegraph.py
@@ -1,10 +1,8 @@
-import matplotlib.pyplot as plt
 import csv
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.misc import imread
-import matplotlib.cbook as cbook
+import matplotlib.image as mpimg
 import os
 
 
@@ -18,14 +16,16 @@ with open(sys.argv[1],'r') as csvfile:
         y.append(float(row[1]))
 
 
-img = imread('cave.png')
-plt.scatter(x,y,zorder=1)
-plt.imshow(img, zorder=0, extent=[-8.0, 8.0, -8.0, 8.0]) #cave.png limits
-#plt.legend('Robotaren ibilbidea', scatterpoints=1)
+img = mpimg.imread('cave.png')
+plt.scatter(x, y, s=3, zorder=1)
+plt.imshow(img, zorder=0, extent=[-8.0, 8.0, -8.0, 8.0], cmap='gray', vmin=0, vmax=1) #cave.png limits
+plt.legend('Robotaren ibilbidea', scatterpoints=1)
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Cave mapa')
-#plt.show()
-base=os.path.basename(sys.argv[1])
-plt.savefig('results/'+os.path.splitext(base)[0])
+#plt.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False, labelbottom=False, labeltop=False, labelleft=False, labelright=False)
+plt.gca().legend().set_visible(False)
+plt.show()
+#base=os.path.basename(sys.argv[1])
+#plt.savefig('results/'+os.path.splitext(base)[0])
 
