97945e6aa5078bdda90d8d84cb8a9141d6bd5798
Xabier Lahuerta Vázquez <xabilahu99@hotmail.com>
2020-11-10 19:30:15 +0100

    Some results + Sectioning fixed

--- PlayerStage/oekidin/graphs/makegraph.py
+++ PlayerStage/oekidin/graphs/makegraph.py
@@ -14,15 +14,28 @@ with open(sys.argv[1],'r') as csvfile:
         x.append(float(row[0]))
         y.append(float(row[1]))
 
-img = mpimg.imread('cave.png')
-plt.scatter(x, y, s=1, zorder=1)
-plt.imshow(img, zorder=0, extent=[-8.0, 8.0, -8.0, 8.0], cmap='gray', vmin=0, vmax=1) #cave.png limits
+if len(sys.argv) > 2 :
+    img = mpimg.imread('hospital.png')
+    ext = [-70, 70, -25, 25]
+    plt.rc('font', size=4)
+else:
+    img = mpimg.imread('cave.png')
+    ext = [-8.0, 8.0, -8.0, 8.0]
+plt.scatter(x, y, s=0.1, zorder=1)
+plt.imshow(img, zorder=0, extent=ext, cmap='gray', vmin=0, vmax=1) #cave.png limits
 if (np.array_equal(x[-5:-1], x[-10:-6]) and np.array_equal(y[-5:-1], y[-10:-6])):
     plt.scatter(x[-1],y[-1],zorder=1,c='r')
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Cave mapa')
+
+if len(sys.argv) > 2 :
+    plt.title('Ospital mapa')
+else:
+    plt.title('Cave mapa')
+
+plt.tight_layout()
+#plt.show()
 #plt.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False, labelbottom=False, labeltop=False, labelleft=False, labelright=False)
 base=os.path.basename(sys.argv[1])
-plt.savefig('results/'+os.path.splitext(base)[0])
+plt.savefig('results/'+os.path.splitext(base)[0], dpi=500, bbox_inches='tight')
 
