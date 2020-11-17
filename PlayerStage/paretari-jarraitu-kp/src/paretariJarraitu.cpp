31258ddf132727a846a6c785a258f4c7d22ca7f4
Xabier Lahuerta Vázquez <xabilahu99@hotmail.com>
2020-11-17 11:33:10 +0100

    Reformat code

--- PlayerStage/paretari-jarraitu-kp/src/paretariJarraitu.cpp
+++ PlayerStage/paretari-jarraitu-kp/src/paretariJarraitu.cpp
@@ -11,22 +11,19 @@ std::ofstream datuFitx;
 
 using namespace std;
 
-
-void
-closeAll(int signum)
+void closeAll(int signum)
 {
   std::cout << "Interrupt signal (" << signum << ") received\n";
   datuFitx.close();
   exit(signum);
 }
 
-int
-main(int argc, const char **argv)
+int main(int argc, const char **argv)
 {
   int i, ind;
   float diff;
   float batezbdist;
-  float v,w;
+  float v, w;
   float dist;
   float Kp;
 
@@ -34,21 +31,21 @@ main(int argc, const char **argv)
 
   signal(SIGINT, closeAll);
   if (argc < 3)
-    {
-      std::cout << "Robotak paretari jarraituko dio agindutako" << std::endl;
-      std::cout << "distantzia mantenduz." << std::endl;
-      std::cout << "Erabilera: " << argv[0] <<" <distantzia> <Kp> [<outFitx>]" << std::endl;
-      return -1;
-    }
+  {
+    std::cout << "Robotak paretari jarraituko dio agindutako" << std::endl;
+    std::cout << "distantzia mantenduz." << std::endl;
+    std::cout << "Erabilera: " << argv[0] << " <distantzia> <Kp> [<outFitx>]" << std::endl;
+    return -1;
+  }
   dist = atof(argv[1]);
   Kp = atof(argv[2]);
   if (argc == 4)
     fileName = argv[3];
   datuFitx.open(fileName, ios::out);
-  
+
   std::cout << "Distantzia: " << dist << " Kp: " << Kp << " outFileName: " << fileName << std::endl;
-  datuFitx  << "# Distantzia: " << dist << " Kp: " << Kp  << "\n";
-  datuFitx  << "# BatezbDist  Errorea  w v X Y\n";
+  datuFitx << "# Distantzia: " << dist << " Kp: " << Kp << "\n";
+  datuFitx << "# BatezbDist  Errorea  w v X Y\n";
 
   try
   {
@@ -60,37 +57,32 @@ main(int argc, const char **argv)
     LaserProxy laserra(&bezeroa, 0);
     // Sortu position2d interfazea eta harpidetu bezeroarekin
     Position2dProxy robota(&bezeroa, 0);
-    
+
     std::cout << bezeroa << std::endl;
-    
-    robota.SetMotorEnable (true);
+
+    robota.SetMotorEnable(true);
 
     for (i = 0; i < 10; i++)
       bezeroa.Read();
-  
+
     while (1)
-      {
-	batezbdist = 0;
-	bezeroa.Read();
-	diff = 0
-	/* Kalkulatu paretarako batezbesteko distantzia */
-	  
-	/* Kalkulatu errorea */
-	  
-	/* Abiadurak finkatu proportzionalki */
-	
-	robota.SetSpeed(v, w);
-	datuFitx << batezbdist << " " << diff << " " << w << " " << v << " " << robota.GetXPos() << " " << robota.GetYPos() << "\n"; 
-
-      }
-    
-  }
-  catch (PlayerCc::PlayerError & e)
     {
-      std::cerr << e << std::endl;
-      return -1;
+      batezbdist = 0;
+      bezeroa.Read();
+      diff = 0
+             /* Kalkulatu paretarako batezbesteko distantzia */
+
+             /* Kalkulatu errorea */
+
+             /* Abiadurak finkatu proportzionalki */
+
+             robota.SetSpeed(v, w);
+      datuFitx << batezbdist << " " << diff << " " << w << " " << v << " " << robota.GetXPos() << " " << robota.GetYPos() << "\n";
     }
-    
+  }
+  catch (PlayerCc::PlayerError &e)
+  {
+    std::cerr << e << std::endl;
+    return -1;
+  }
 }
-
-
